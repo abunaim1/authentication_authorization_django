a99d2400c8358e92547487339225d30d9558971b
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2024-01-24 21:45:56 +0600

    done

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,11 +1,17 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
     email = forms.CharField(required=True)
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+class ChangeUserData(UserChangeForm):
+    password = None
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
--- first_app/views.py
+++ first_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect 
-from .forms import RegisterForm
+from .forms import RegisterForm, ChangeUserData
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, SetPasswordForm # here we found user name and password
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
@@ -42,9 +42,16 @@ def user_login(request):
 
 def profile(request):
     if request.user.is_authenticated:
-        return render(request, 'profile.html', {'user':request.user})
+        if request.method == 'POST':
+            form = ChangeUserData(request.POST, instance = request.user)
+            if form.is_valid():
+                messages.success(request, 'Account updated successfully!')
+                form.save()
+        else:
+            form = ChangeUserData( instance = request.user)
+        return render(request, 'profile.html', {'form':form})
     else:
-        return redirect('login')
+        return redirect('signup ')
 
 def user_logout(request):
     logout(request)
@@ -79,3 +86,4 @@ def pass_change2(request):
         return render(request, 'passchange.html', {'form':form})
     else:
          return redirect('login')
+    
