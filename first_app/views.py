6e67ca1fab135ceab742cde220c15af310c4a013
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2024-01-24 10:33:16 +0600

    form added success

--- first_app/views.py
+++ first_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .forms import RegisterForm
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
+            messages.success(request, 'Account created successfully!')
             form.save()
     else:
         form = RegisterForm()
